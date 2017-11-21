aa11ac011340c367dce7443f051f14a75902eeff
Daisuke Inoue <hibariyashiki@gmail.com>
2017-11-21 13:16:44 +0900

    I'm satisfied

--- Akihamiti.el
+++ Akihamiti.el
@@ -55,6 +55,42 @@
     ""
     :group 'powerline)
 
+(defface Akihamiti-green-var
+    `((t
+          (
+              :foreground "white"
+              :background ,monokai-green
+              :inherit mode-line)))
+    ""
+    :group 'powerline)
+
+(defface Akihamiti-red-var
+    `((t
+          (
+              :foreground "white"
+              :background ,monokai-red
+              :inherit mode-line)))
+    ""
+    :group 'powerline)
+
+(defface Akihamiti-cyan-var
+    `((t
+          (
+              :foreground "white"
+              :background ,monokai-cyan
+              :inherit mode-line)))
+    ""
+    :group 'powerline)
+
+(defface Akihamiti-blue-var
+    `((t
+          (
+              :foreground "white"
+              :background ,monokai-blue
+              :inherit mode-line)))
+    ""
+    :group 'powerline)
+
 (defface Akihamiti-red
     `((t
           (
@@ -85,7 +121,14 @@
                          (mode-line-buffer-id (if active 'mode-line-buffer-id 'mode-line-buffer-id-inactive))
                          (mode-line (if active 'mode-line 'mode-line-inactive))
                          (face0 (if active 'Akihamiti-green 'powerline-inactive2))
-                         (face0var (if active 'Akihamiti-green 'powerline-inactive2))
+                         (face0var
+                             (if
+                                 active
+                                 (if
+                                     (buffer-modified-p)
+                                     'Akihamiti-red-var
+                                     'Akihamiti-blue-var)
+                                 'powerline-inactive2))
                          (face1 (if active 'Akihamiti-red 'powerline-inactive2))
                          (face2 (if active 'Akihamiti-blue 'powerline-inactive2))
                          (face3 (if active 'ieremii-giant-goldfish 'powerline-inactive2))
@@ -104,11 +147,12 @@
                          (lhs
                              (list
                                  (when powerline-display-mule-info
-                                     (powerline-raw mode-line-mule-info face0var 'l))
-                                 (powerline-raw "%*" face0 'l)
-                                 (funcall separator-left face0 face2)
+                                     (powerline-raw mode-line-mule-info face0 'l))
+                                 (funcall separator-left face0 face0var)
+                                 (powerline-raw "%* " face0var 'l)
+                                 (funcall separator-left face0var face0)
                                  (when powerline-display-buffer-size
-                                     (powerline-buffer-size face2 'l))
+                                     (powerline-buffer-size face0 'l))
                                  (powerline-raw " " face0)
                                  (powerline-raw buffer face0)
                                  (when (and (boundp 'which-func-mode) which-func-mode)
@@ -119,26 +163,27 @@
                                  (powerline-process face0)
                                  (powerline-narrow face0 'l)
                                  (powerline-vc face0 'r)
-                                 (powerline-raw (format "%s" (get-char-property (point) 'face)) face2 'l)
+                                 (funcall separator-left face0 face2)
+                                 (powerline-raw (format "%s " (get-char-property (point) 'face)) face2 'l)
+                                 (funcall separator-left face2 face0)
                                  (when (bound-and-true-p nyan-mode)
                                      (powerline-raw (list (nyan-create)) face0 'l))))
                          (rhs
                              (list
                                  (powerline-raw global-mode-string face3 'r)
-                                 ;;          (funcall separator-right face2 face1)
+                                 (funcall separator-right face0 face1)
                                  (unless window-system
                                      (powerline-raw (char-to-string #xe0a1) face4 'l))
                                  (powerline-raw "%4l" face1 'l)
                                  (powerline-raw ":" face1 'l)
                                  (powerline-raw "%3c" face1 'r)
-                                 ;;          (funcall separator-right face1 mode-line)
-                                 (powerline-raw " " face2)
-                                 (powerline-raw "%6p" face1 'r)
+                                 (funcall separator-right face1 face0)
+                                 (powerline-raw " %6p" face0 'r)
                                  (when powerline-display-hud
-                                     (powerline-hud face3 face4 1)))))
+                                     (powerline-hud face0var face1 2)))))
                      (concat
                          (powerline-render lhs)
-                         (powerline-fill face2 (powerline-width rhs))
+                         (powerline-fill face0 (powerline-width rhs))
                          (powerline-render rhs)))))))
 (powerline-Akihamiti)
 
